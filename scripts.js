d6e8bc6eefa302cf1fea5d5210212bebebc8e506
JunK-enter <73394528+JunK-enter@users.noreply.github.com>
2023-07-27 22:51:29 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -56,3 +56,61 @@ let currentPage = 1;
   document.addEventListener("DOMContentLoaded", () => {
     updatePage();
   });
+
+   // Function to make requests to the Google Analytics Reporting API V4.
+   async function requestGAData(page_id, access_token, start_date, end_date) {
+    const response = await fetch(`https://www.googleapis.com/analytics/v3/data/ga?ids=ga:${page_id}&start_date=${start_date}&end_date=${end_date}&metrics=ga%3Apageviews`, {
+      headers: {
+        "Authorization": `Bearer ${access_token}`
+      }
+    });
+
+    const data = await response.json();
+    if (data.error) {
+      console.error(data.error);
+      return null;
+    }
+
+    return data;
+  }
+
+  async function requestGAData(page_id, access_token, start_date, end_date) {
+    const response = await fetch(`https://www.googleapis.com/analytics/v3/data/ga?ids=ga:${page_id}&start_date=${start_date}&end_date=${end_date}&metrics=ga%3Apageviews`, {
+      headers: {
+        "Authorization": `Bearer ${access_token}`
+      }
+    });
+
+    const data = await response.json();
+    if (data.error) {
+      console.error(data.error);
+      return null;
+    }
+
+    return data;
+  }
+
+  async function getTotalVisitors() {
+    const access_token = "ACCESS_TOKEN";
+    const view_id = "VIEW_ID";
+    const page_id = "YOUR_MEASUREMENT_ID";
+
+    const now = new Date();
+    const start_date = "2005-01-01";
+    const end_date = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
+
+    const total_data = await requestGAData(page_id, access_token, start_date, end_date);
+    const total_visitors = total_data && total_data.rows && total_data.rows[0] && parseInt(total_data.rows[0][0], 10);
+
+    return total_visitors;
+  }
+
+  function updateTotalVisitorCount() {
+    getTotalVisitors().then((total_visitors) => {
+      document.getElementById("total-visitors").innerText = total_visitors;
+    }).catch((error) => {
+      console.error("Error fetching visitor data:", error);
+    });
+  }
+
+  updateTotalVisitorCount();
